4bb580529d1a4a8421b5157b2a3b8bc2dcc67988
Alex Subota <as.subota@gmail.com>
2015-10-10 16:33:56 +0300

    Stub for submit search function

--- src/app/components/search/Search.js
+++ src/app/components/search/Search.js
@@ -1,11 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component, findDOMNode, PropTypes } from 'react';
+import emptyFunction from 'react/lib/emptyFunction';
 
 export default class Search extends Component {
 
+    static propTypes = {
+        onSubmit: PropTypes.func
+    };
+
+    static defaultTypes = {
+        onSubmit: emptyFunction
+    };
+
+    onFormSubmit = (e) => {
+        e.preventDefault();
+        console.log(findDOMNode(this.term).value);
+        this.props.onSubmit(findDOMNode(this.term).value);
+    };
+
     render() {
         return (
-            <form>
-                <input type="text" />
+            <form onSubmit={this.onFormSubmit}>
+                <input type="text" ref={c => this.term = c} />
 
                 <button type="submit">Search</button>
             </form>
