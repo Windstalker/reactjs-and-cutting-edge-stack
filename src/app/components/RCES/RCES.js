f99eae40f1b5465e5da42b48c06cb027b93e7e57
Alex Subota <as.subota@gmail.com>
2015-10-10 16:44:49 +0300

    Github Repo search on form submit

--- src/app/components/RCES/RCES.js
+++ src/app/components/RCES/RCES.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import Search from 'components/search/Search';
+import { search as gitHubSearch } from 'utils/GITHUBAPIUtils';
 
 export default class RCES extends Component {
+
+    async onSearch(term) {
+        const results = await gitHubSearch(term);
+
+        console.log(results);
+    };
 
     render() {
         return (
             <div>
                 <h1>Github Search</h1>
 
-                <Search />
+                <Search onSubmit={this.onSearch} />
             </div>
         );
     }
